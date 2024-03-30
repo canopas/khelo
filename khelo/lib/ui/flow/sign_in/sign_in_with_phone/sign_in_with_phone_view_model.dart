e1385da3fbaa835f5bb4808f41aedf978d941072
cp-sneha-s <sneha.s@canopas.com>
2024-03-30 12:41:23 +0530

    Fix phone authentication

--- khelo/lib/ui/flow/sign_in/sign_in_with_phone/sign_in_with_phone_view_model.dart
+++ khelo/lib/ui/flow/sign_in/sign_in_with_phone/sign_in_with_phone_view_model.dart
@@ -66,6 +66,7 @@ class SignInWithPhoneViewNotifier extends StateNotifier<SignInWithPhoneState> {
             state = state.copyWith(verifying: false, signInSuccess: true);
           },
           onVerificationFailed: (error) {
+            print(error);
             state = state.copyWith(verifying: false, error: error);
           },
           onCodeSent: (verificationId, _) {
