e62452357babfe0c4091ea335c50623a322f889f
sidhdhi canopas <sidhdhi.p@canopas.com>
2024-05-21 10:50:36 +0530

    redesign login screens

--- khelo/lib/ui/flow/sign_in/sign_in_with_phone/components/sign_in_with_phone_country_picker.dart
+++ khelo/lib/ui/flow/sign_in/sign_in_with_phone/components/sign_in_with_phone_country_picker.dart
@@ -1,8 +1,10 @@
 import 'package:canopas_country_picker/canopas_country_picker.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:khelo/gen/assets.gen.dart';
+import 'package:style/animations/on_tap_scale.dart';
 import 'package:style/extensions/context_extensions.dart';
 import 'package:style/text/app_text_style.dart';
 
@@ -18,69 +20,69 @@ class SignInWithPhoneCountryPicker extends ConsumerWidget {
       signInWithPhoneStateProvider.select((value) => value.code),
     );
 
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: GestureDetector(
-            onTap: () async {
-              final countryCode = await showCountryCodePickerSheet(
-                customizationBuilders: CustomizationBuilders(
-                  textFieldBuilder: (filter) =>
-                      DefaultCountryCodeFilterTextField(
-                    prefixIcon: Icon(
-                      CupertinoIcons.search,
-                      color: context.colorScheme.textPrimary,
-                      size: 22,
-                    ),
-                    fillColor: context.colorScheme.containerLow,
-                    style: AppTextStyle.body1
-                        .copyWith(color: context.colorScheme.textPrimary),
-                    hintStyle: AppTextStyle.body1
-                        .copyWith(color: context.colorScheme.textDisabled),
-                    filter: filter,
-                  ),
-                  codeBuilder: (code) => GestureDetector(
-                    onTap: () => context.pop(code),
-                    child: DefaultCountryCodeListItemView(
-                      code: code,
-                      dialCodeStyle: AppTextStyle.body1
-                          .copyWith(color: context.colorScheme.textPrimary),
-                      nameStyle: AppTextStyle.body1
-                          .copyWith(color: context.colorScheme.textPrimary),
-                    ),
-                  ),
-                  backgroundColor: () => context.colorScheme.surface,
-                ),
-                context: context,
-              );
-              if (countryCode != null) {
-                notifier.changeCountryCode(countryCode);
-              }
-            },
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  countryCode.dialCode,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 2),
-                  child: Icon(
-                    Icons.keyboard_arrow_down_rounded,
-                    size: 24,
-                    color: context.colorScheme.textSecondary,
-                  ),
-                ),
-              ],
-            ),
+    return OnTapScale(
+      onTap: () => _showCountryPickerSheet(context, notifier),
+      child: Row(
+        children: [
+          const SizedBox(width: 16),
+          Text(
+            countryCode.dialCode,
+            style: AppTextStyle.body1
+                .copyWith(color: context.colorScheme.textSecondary),
+          ),
+          SvgPicture.asset(
+            Assets.images.icArrowDown,
+            colorFilter: ColorFilter.mode(
+                context.colorScheme.textSecondary, BlendMode.srcATop),
+            height: 14,
+            width: 14,
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showCountryPickerSheet(
+      BuildContext context, SignInWithPhoneViewNotifier notifier) async {
+    final countryCode = await showCountryCodePickerSheet(
+      customizationBuilders: _getCustomizationBuilder(context),
+      context: context,
+    );
+    if (countryCode != null) {
+      notifier.changeCountryCode(countryCode);
+    }
+  }
+
+  CustomizationBuilders _getCustomizationBuilder(BuildContext context) {
+    return CustomizationBuilders(
+      backgroundColor: () => context.colorScheme.surface,
+      textFieldBuilder: (filter) => DefaultCountryCodeFilterTextField(
+        filter: filter,
+        fillColor: context.colorScheme.containerLow,
+        prefixIcon: Icon(
+          CupertinoIcons.search,
+          color: context.colorScheme.textSecondary,
+          size: 22,
+        ),
+        style: AppTextStyle.body1.copyWith(
+          color: context.colorScheme.textPrimary,
+        ),
+        hintStyle: AppTextStyle.body1.copyWith(
+          color: context.colorScheme.textSecondary,
+        ),
+      ),
+      codeBuilder: (code) => GestureDetector(
+        onTap: () => context.pop(code),
+        child: DefaultCountryCodeListItemView(
+          code: code,
+          dialCodeStyle: AppTextStyle.body1.copyWith(
+            color: context.colorScheme.textPrimary,
+          ),
+          nameStyle: AppTextStyle.body1.copyWith(
+            color: context.colorScheme.textPrimary,
           ),
         ),
-        const SizedBox(
-          height: 30,
-          width: 1,
-        ),
-      ],
+      ),
     );
   }
 }
