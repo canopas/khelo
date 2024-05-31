294770dbd46b9c6d372f000bbb99c5a3a3867aeb
cp-mayank-v <mayank.v@canopas.com>
2024-05-31 18:52:46 +0530

    Redesign select option sheet

--- khelo/lib/gen/assets.gen.dart
+++ khelo/lib/gen/assets.gen.dart
@@ -7,8 +7,6 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
-import 'package:flutter/widgets.dart';
-
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
@@ -30,9 +28,8 @@ class $AssetsImagesGen {
   /// File path: assets/images/ic_calendar.svg
   String get icCalendar => 'assets/images/ic_calendar.svg';
 
-  /// File path: assets/images/ic_camera.png
-  AssetGenImage get icCamera =>
-      const AssetGenImage('assets/images/ic_camera.png');
+  /// File path: assets/images/ic_camera.svg
+  String get icCamera => 'assets/images/ic_camera.svg';
 
   /// File path: assets/images/ic_check.svg
   String get icCheck => 'assets/images/ic_check.svg';
@@ -43,9 +40,8 @@ class $AssetsImagesGen {
   /// File path: assets/images/ic_edit.svg
   String get icEdit => 'assets/images/ic_edit.svg';
 
-  /// File path: assets/images/ic_gallery.png
-  AssetGenImage get icGallery =>
-      const AssetGenImage('assets/images/ic_gallery.png');
+  /// File path: assets/images/ic_gallery.svg
+  String get icGallery => 'assets/images/ic_gallery.svg';
 
   /// File path: assets/images/ic_group.svg
   String get icGroup => 'assets/images/ic_group.svg';
@@ -90,7 +86,7 @@ class $AssetsImagesGen {
   String get tennisBall => 'assets/images/tennis_ball.svg';
 
   /// List of all assets
-  List<dynamic> get values => [
+  List<String> get values => [
         batsman,
         bowler,
         icArrowDown,
@@ -123,80 +119,4 @@ class Assets {
   Assets._();
 
   static const $AssetsImagesGen images = $AssetsImagesGen();
-}
-
-class AssetGenImage {
-  const AssetGenImage(this._assetName, {this.size = null});
-
-  final String _assetName;
-
-  final Size? size;
-
-  Image image({
-    Key? key,
-    AssetBundle? bundle,
-    ImageFrameBuilder? frameBuilder,
-    ImageErrorWidgetBuilder? errorBuilder,
-    String? semanticLabel,
-    bool excludeFromSemantics = false,
-    double? scale,
-    double? width,
-    double? height,
-    Color? color,
-    Animation<double>? opacity,
-    BlendMode? colorBlendMode,
-    BoxFit? fit,
-    AlignmentGeometry alignment = Alignment.center,
-    ImageRepeat repeat = ImageRepeat.noRepeat,
-    Rect? centerSlice,
-    bool matchTextDirection = false,
-    bool gaplessPlayback = false,
-    bool isAntiAlias = false,
-    String? package,
-    FilterQuality filterQuality = FilterQuality.low,
-    int? cacheWidth,
-    int? cacheHeight,
-  }) {
-    return Image.asset(
-      _assetName,
-      key: key,
-      bundle: bundle,
-      frameBuilder: frameBuilder,
-      errorBuilder: errorBuilder,
-      semanticLabel: semanticLabel,
-      excludeFromSemantics: excludeFromSemantics,
-      scale: scale,
-      width: width,
-      height: height,
-      color: color,
-      opacity: opacity,
-      colorBlendMode: colorBlendMode,
-      fit: fit,
-      alignment: alignment,
-      repeat: repeat,
-      centerSlice: centerSlice,
-      matchTextDirection: matchTextDirection,
-      gaplessPlayback: gaplessPlayback,
-      isAntiAlias: isAntiAlias,
-      package: package,
-      filterQuality: filterQuality,
-      cacheWidth: cacheWidth,
-      cacheHeight: cacheHeight,
-    );
-  }
-
-  ImageProvider provider({
-    AssetBundle? bundle,
-    String? package,
-  }) {
-    return AssetImage(
-      _assetName,
-      bundle: bundle,
-      package: package,
-    );
-  }
-
-  String get path => _assetName;
-
-  String get keyName => _assetName;
 }
