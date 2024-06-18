1556f35f9ec9f29c54ba2be745419c0598508b0f
Jimmy Sanghani <jimmy0251@gmail.com>
2024-06-18 16:50:24 +0530

    Add apple push notification config (#45)
    * Add apple push notification config
    
    * install flutterfire in ci
    
    * Minor changes
    
    * Configure project
    
    * Minor changes
    
    * Possible fix for phone authentication
    
    * Possible fix for phone authentication in android
    
    * Possible fix for build in ios
    
    * Possible fix for build in android
    
    * Possible fix for build in ios
    
    * Fix lint error
    
    * Fix lint error
    
    * upload artifact
    
    * Minor changes
    
    * list files available
    
    * provide exact path for artifact
    
    * provide exact path for artifact
    
    * change path
    
    * root directory file
    
    * move file to root
    
    * move file to root
    
    * move file to root
    
    * remove unused code
    
    * change aab path
    
    * change aab path
    
    * change action
    
    * add distribution
    
    * run job on main
    
    * change java version
    
    * run on push
    
    * change java version
    
    * run on main
    
    * rm flutter fire cli
    
    * run on main
    
    ---------
    
    Co-authored-by: sidhdhi canopas <sidhdhi.p@canopas.com>
    Co-authored-by: cp-sneha-s <sneha.s@canopas.com>

--- khelo/ios/Runner/AppDelegate.swift
+++ khelo/ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
-import FirebaseAuth
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,21 +7,7 @@ import FirebaseAuth
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    let firebaseAuth = Auth.auth()
-    firebaseAuth.setAPNSToken(deviceToken, type: AuthAPNSTokenType.unknown)
-  }
-
-  override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-    let firebaseAuth = Auth.auth()
-    if (firebaseAuth.canHandleNotification(userInfo)){
-        print(userInfo)
-        return
-    }
   }
 }
