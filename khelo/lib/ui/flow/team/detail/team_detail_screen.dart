649fcb1e4a21ecfbb80f0488c0751622fae056b7
sidhdhi canopas <sidhdhi.p@canopas.com>
2024-06-27 15:36:52 +0530

    remove unnecessary import

--- khelo/lib/ui/flow/team/detail/team_detail_screen.dart
+++ khelo/lib/ui/flow/team/detail/team_detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:khelo/components/action_bottom_sheet.dart';
