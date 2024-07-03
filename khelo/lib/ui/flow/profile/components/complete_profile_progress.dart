a6d0754f4f2b1ec4428f2398b46466d132b5a296
cp-mayank-v <mayank.v@canopas.com>
2024-07-03 11:21:58 +0530

    Mr changes

--- khelo/lib/ui/flow/profile/components/complete_profile_progress.dart
+++ khelo/lib/ui/flow/profile/components/complete_profile_progress.dart
@@ -2,6 +2,8 @@ import 'package:data/api/user/user_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:khelo/domain/extensions/context_extensions.dart';
+import 'package:khelo/ui/app_route.dart';
+import 'package:style/animations/on_tap_scale.dart';
 import 'package:style/extensions/context_extensions.dart';
 import 'package:style/text/app_text_style.dart';
 
@@ -25,51 +27,54 @@ class _CompleteProfileProgressState
         widget.user?.batting_style != null &&
         widget.user?.bowling_style != null) return const SizedBox();
 
-    return Container(
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.only(top: 24),
-      decoration: BoxDecoration(
-        border: Border.all(color: context.colorScheme.outline),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            context.l10n.profile_complete_your_profile_title,
-            style: AppTextStyle.header4.copyWith(
-              color: context.colorScheme.textPrimary,
+    return OnTapScale(
+      onTap: () => AppRoute.editProfile().push(context),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.only(top: 24),
+        decoration: BoxDecoration(
+          border: Border.all(color: context.colorScheme.outline),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.l10n.profile_complete_your_profile_title,
+              style: AppTextStyle.header4.copyWith(
+                color: context.colorScheme.textPrimary,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            context.l10n.profile_complete_description_title,
-            style: AppTextStyle.subtitle3.copyWith(
-              color: context.colorScheme.textSecondary,
+            const SizedBox(height: 8),
+            Text(
+              context.l10n.profile_complete_description_title,
+              style: AppTextStyle.subtitle3.copyWith(
+                color: context.colorScheme.textSecondary,
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          LinearProgressIndicator(
-            value: widget.user?.progress,
-            minHeight: 6,
-            borderRadius: BorderRadius.circular(12),
-            backgroundColor: context.colorScheme.outline,
-            valueColor:
-                AlwaysStoppedAnimation<Color>(context.colorScheme.primary),
-          ),
-          const SizedBox(height: 8),
-          if (widget.user?.profile_img_url == null) ...[
-            _fieldStatus(context.l10n.profile_complete_add_profile_picture),
+            const SizedBox(height: 16),
+            LinearProgressIndicator(
+              value: widget.user?.progress,
+              minHeight: 6,
+              borderRadius: BorderRadius.circular(12),
+              backgroundColor: context.colorScheme.outline,
+              valueColor:
+                  AlwaysStoppedAnimation<Color>(context.colorScheme.primary),
+            ),
+            const SizedBox(height: 8),
+            if (widget.user?.profile_img_url == null) ...[
+              _fieldStatus(context.l10n.profile_complete_add_profile_picture),
+            ],
+            if (widget.user?.dob == null || widget.user?.gender == null) ...[
+              _fieldStatus(context.l10n.profile_complete_add_personal_details),
+            ],
+            if (widget.user?.player_role == null ||
+                widget.user?.batting_style == null ||
+                widget.user?.bowling_style == null) ...[
+              _fieldStatus(context.l10n.profile_complete_add_playing_style),
+            ],
           ],
-          if (widget.user?.dob == null || widget.user?.gender == null) ...[
-            _fieldStatus(context.l10n.profile_complete_add_personal_details),
-          ],
-          if (widget.user?.player_role == null ||
-              widget.user?.batting_style == null ||
-              widget.user?.bowling_style == null) ...[
-            _fieldStatus(context.l10n.profile_complete_add_playing_style),
-          ],
-        ],
+        ),
       ),
     );
   }
@@ -80,14 +85,14 @@ class _CompleteProfileProgressState
       child: Row(
         children: [
           Icon(
-            Icons.radio_button_off_rounded,
-            color: context.colorScheme.textSecondary,
+            Icons.check_circle_rounded,
+            color: context.colorScheme.containerHigh,
           ),
           const SizedBox(width: 16),
           Text(
             title,
-            style: AppTextStyle.subtitle2.copyWith(
-              color: context.colorScheme.textPrimary,
+            style: AppTextStyle.body1.copyWith(
+              color: context.colorScheme.textSecondary,
             ),
           )
         ],
