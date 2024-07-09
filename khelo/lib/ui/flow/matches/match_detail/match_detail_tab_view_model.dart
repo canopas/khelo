28e061c079f9548467dfbb2969f35b21e46d7430
cp-mayank-v <mayank.v@canopas.com>
2024-07-09 13:06:38 +0530

    Fix scorecard expanded issue

--- khelo/lib/ui/flow/matches/match_detail/match_detail_tab_view_model.dart
+++ khelo/lib/ui/flow/matches/match_detail/match_detail_tab_view_model.dart
@@ -81,7 +81,7 @@ class MatchDetailTabViewNotifier extends StateNotifier<MatchDetailTabState> {
             .firstOrNull;
 
         onScorecardExpansionChange(
-          match.matchResult?.teamId ?? match.teams.last.team.id ?? "",
+          match.matchResult?.teamId ?? match.current_playing_team_id?? "",
           true,
         );
         state = state.copyWith(
