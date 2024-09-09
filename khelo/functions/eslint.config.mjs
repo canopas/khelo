a38f3f48a5c73c272fac560d2298b29271e6362a
sidhdhi canopas <122426509+cp-sidhdhi-p@users.noreply.github.com>
2024-09-09 14:22:26 +0530

    Cloud functions (#93)
    * init functions
    
    * write functions
    
    * minor changes
    
    * fix team notification
    
    * refactor log
    
    * deploy func using ci
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * run on main branch

---
+++ khelo/functions/eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from "globals";
+import js from "@eslint/js";
+import google from "eslint-config-google";
+
+export default [
+  js.configs.recommended,
+  google,
+  {
+    languageOptions: {
+      ecmaVersion: 2018,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        firebase: true,
+        admin: true,
+      },
+    },
+    rules: {
+      "no-restricted-globals": ["error", "name", "length"],
+      "prefer-arrow-callback": "error",
+      "quotes": ["error", "double"],
+      "import/no-unresolved": 0,
+      "indent": ["error", 2],
+      "max-len": ["error", {"code": 140}],
+      "new-cap": 0,
+      "require-jsdoc": 0,
+      "no-extend-native": 0,
+      "camelcase": 0,
+    },
+  },
+  {
+    files: ["**/*.spec.*"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+    rules: {},
+  }];
---
+++ khelo/functions/src/index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {value: true});
+exports.fiveMinuteCron = exports.teamPlayerChangeObserver = exports.TIMEZONE = void 0;
+
+const app_1 = require("firebase-admin/app");
+const firestore_1 = require("firebase-admin/firestore");
+const firestore_2 = require("firebase-functions/v2/firestore");
+const scheduler = require("firebase-functions/v2/scheduler");
+
+const team_repository = require("./team/team_repository");
+const user_repository = require("./user/user_repository");
+const match_repository = require("./match/match_repository");
+
+const notification_service = require("./notification/notification_service");
+const team_service = require("./team/team_service");
+const match_service = require("./match/match_service");
+
+const logger = require("firebase-functions/logger");
+
+exports.TIMEZONE = "Asia/Kolkata";
+const REGION = "asia-south1";
+const app = (0, app_1.initializeApp)();
+const db = (0, firestore_1.getFirestore)(app);
+
+const userRepository = new user_repository.UserRepository(db);
+const teamRepository = new team_repository.TeamRepository(db);
+
+const notificationService = new notification_service.NotificationService(userRepository);
+const teamService = new team_service.TeamService(userRepository, notificationService);
+const matchService = new match_service.MatchService(userRepository, teamRepository, notificationService);
+
+const matchRepository = new match_repository.MatchRepository(db, matchService);
+
+exports.teamPlayerChangeObserver = (0, firestore_2.onDocumentUpdated)({region: REGION, document: "teams/{teamId}"}, async (event) => {
+  const snapshot = event.data;
+  if (!snapshot) {
+    (0, logger.error)("No data associated with the event");
+    return;
+  }
+  const oldTeam = snapshot.before.data();
+  const newTeam = snapshot.after.data();
+
+  await teamService.notifyOnAddedToTeam(oldTeam, newTeam);
+});
+
+exports.fiveMinuteCron = (0, scheduler.onSchedule)({timeZone: exports.TIMEZONE, schedule: "*/5 * * * *"}, async () => {
+  await matchRepository.processUpcomingMatches();
+});
