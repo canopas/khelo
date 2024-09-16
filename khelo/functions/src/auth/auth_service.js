b873db2ea37bf7062efa8f92b6fd79e62a023b43
sidhdhi canopas <sidhdhi.p@canopas.com>
2024-09-16 13:16:02 +0530

    refactor logs

--- khelo/functions/src/auth/auth_service.js
+++ khelo/functions/src/auth/auth_service.js
@@ -9,10 +9,12 @@ class AuthService {
   async onCallCreateAuthUser(req, res) {
     const userId = req.headers["auth-uid"];
     if (!userId) {
+      console.error("AuthService: unauthorized request");
       return res.status(400).send("unauthorized");
     }
     const request = req.body;
     if (!request.phone || !request.name) {
+      console.error("AuthService: missing parameters");
       return res.status(400).send("missing-params");
     }
     const userRecord = await firebase_admin.auth().createUser({
@@ -20,7 +22,7 @@ class AuthService {
       displayName: request.name,
     });
     const user = await this.userRepository.createUser(userRecord.uid, request.name, request.phone);
-    console.log("User has been created", user);
+    console.log("AuthService: User has been created", user);
     if (user != null) {
       res.status(200).send(user);
     } else {
